11aa9331eedab4d8f96286e61287534e2bb68f3e
Malhar Deshmukh <malhar_deshmukh@outlook.com>
2018-12-11 11:37:17 +0530

    offline start under test

--- src/firmware/firmware.ino
+++ src/firmware/firmware.ino
@@ -1,4 +1,6 @@
 #include "pindefs.h"
+#include <stdint.h>
+
 //#include <SoftwareSerial.h>
 //SoftwareSerial gsm_serial(gsm_RX_L, gsm_TX_L);
 
@@ -7,7 +9,12 @@
 volatile uint8_t flag_u, flag_v, flag_w;
 volatile int32_t delVU, delWV, delUW, time_preU, time_preV, time_preW, delU, delV, delW;
 
-//remember that there are 120 degree phase shifts, thus when normalised to range of 0-90deg, we get order as YELLOW->RED->BLUE
+int32_t delUW_shadow, delVU_shadow, delWV_shadow, last_phaseOK_stamp, TurnOnWait;
+
+const int32_t timelimitHigh = DEL_TIME_STD + 2000, timelimitLow = DEL_TIME_STD - 2000;
+bool d1Status, d2Status, d3Status, isitfinetoturnon = false;
+int8_t count_sw2 = 0;
+//remember that there are 120 degree phase shifts, thus when normalized to range of 0-90deg, we get order as YELLOW->RED->BLUE
 //thus connection pattern is modified as follows
 //u->YELLOW
 //v->RED
@@ -57,8 +64,84 @@ ISR(PCINT2_vect)
 	}
 }
 
+bool checkPhases()
+{
+	d1Status = ((delUW_shadow <= timelimitHigh) && (delUW_shadow >= timelimitLow));
+	d2Status = ((delVU_shadow <= timelimitHigh) && (delVU_shadow >= timelimitLow));
+	d3Status = ((delWV_shadow <= timelimitHigh) && (delWV_shadow >= timelimitLow));
+
+	return (d1Status && d2Status && d3Status);
+}
+
+void blink(uint8_t _io, int8_t code)
+{
+	switch (code)
+	{
+	case ERROR:
+		flashError(_io);
+		break;
+	case GOOD:
+		flashGood(_io);
+		break;
+	default:
+		break;
+	}
+}
+void flashError(uint8_t _io)
+{
+	ON(_io);
+	delay(ERROR_FLASH_DLY);
+	OFF(_io);
+	delay(ERROR_FLASH_DLY);
+	ON(_io);
+	delay(ERROR_FLASH_DLY);
+	OFF(_io);
+	delay(ERROR_FLASH_DLY);
+	ON(_io);
+	delay(ERROR_FLASH_DLY);
+	OFF(_io);
+	delay(ERROR_FLASH_DLY);
+}
+void flashGood(uint8_t _io)
+{
+	ON(_io);
+	delay(GOOD_FLASH_DLY);
+	OFF(_io);
+	delay(GOOD_FLASH_DLY);
+	ON(_io);
+	delay(GOOD_FLASH_DLY);
+	OFF(_io);
+	delay(GOOD_FLASH_DLY);
+	ON(_io);
+	delay(GOOD_FLASH_DLY);
+	OFF(_io);
+	delay(GOOD_FLASH_DLY);
+}
+void onSequence()
+{
+	if (checkPhases())
+	{
+		ON(rly_ON);
+		delay(RELAY_HOLD);
+		OFF(rly_ON);
+		blink(LED_GREEN, GOOD);
+	}
+	else
+	{
+		blink(LED_RED, ERROR);
+	}
+}
+void offSequence()
+{
+	ON(rly_OFF);
+	delay(1000);
+	OFF(rly_OFF);
+	blink(LED_GREEN2, GOOD);
+}
+
 void setup()
 {
+
 	//external  interrupt setup
 	cli();
 	EICRA |= (1 << ISC11) | (1 << ISC01); //setting int0 and int1 for falling edge,
@@ -88,10 +171,40 @@ void setup()
 	Serial.println(about);
 	delay(500);
 	//gsm_serial.begin(9600);
+
+	TurnOnWait = (((CONFIG == NO_GSM_actualDelay) || (CONFIG == GSM_actualDelay)) ? (analogRead(IN_POT) / 2) : (analogRead(IN_POT))) * 1000; //read the delay value from configuration pot,converting to milliseconds
+
+#ifdef DEBUG_L1
+	Serial.print("TurnOnWait:");
+	Serial.println(TurnOnWait);
+#endif
+	isitfinetoturnon = true;
 }
 void loop()
 {
-	//very inportant loop
+	delUW_shadow = delUW;
+	delVU_shadow = delVU;
+	delWV_shadow = delWV;
+	if (checkPhases())
+	{
+		last_phaseOK_stamp = millis();
+	}
+
+	if (((last_phaseOK_stamp <= (millis() - TurnOnWait)) && isitfinetoturnon) || read(USR_SW1))
+	{
+		onSequence();
+	}
+
+	if (read(USR_SW2))
+	{
+		count_sw2++;
+		if (count_sw2 >= 2)
+		{
+			count_sw2 = 0;
+			offSequence();
+		}
+	}
+
 	//if (flag_u && flag_v && flag_w)
 	// {
 	// 	flag_u = false;
@@ -103,20 +216,25 @@ void loop()
 	// 	delWV = time_preV - time_preW;
 	// }
 
+#ifdef DEBUG_L1
 	Serial.print("delUW: ");
 	Serial.println(delUW);
 	Serial.print("delVU: ");
 	Serial.println(delVU);
 	Serial.print("delWV: ");
 	Serial.println(delWV);
+	Serial.print("checkPhases()");
+	Serial.println(checkPhases());
 
-#ifdef DEBUG_L1
+#ifdef DEBUG_L2
 	Serial.print("time_preU:");
 	Serial.println(time_preU);
 	Serial.print("time_preV:");
 	Serial.println(time_preV);
 	Serial.print("time_preW:");
 	Serial.println(time_preW);
+#endif
+
 #endif
 
 	delay(200);
--- src/firmware/pindefs.h
+++ src/firmware/pindefs.h
@@ -14,7 +14,7 @@
 #define LED_RED 11
 #define LED_GREEN2 12
 #define LED 13
-
+#define IN_POT A5
 #define USR_SW1 20
 #define USR_SW2 21
 
@@ -23,14 +23,27 @@
 #define OUT(X) pinMode(X, OUTPUT)
 #define IN(X) pinMode(X, INPUT)
 #define IN_PU(X) pinMode(X, INPUT_PULLUP)
+
 #define read(X) digitalRead(X)
 
 #define ON(X) digitalWrite(X, 1)
 #define OFF(X) digitalWrite(X, 0)
 
+#define DEL_TIME_STD 6600
+#define RELAY_HOLD 500
+#define ERROR -1
+#define GOOD 2
+
+#define ERROR_FLASH_DLY 50
+#define GOOD_FLASH_DLY 100
 enum config_states
 {
-	normal,
+	NO_GSM_actualDelay = 0,
+	NO_GSM_twiceDelay,
+	GSM_actualDelay,
+	GSM_twiceDelay,
+
+	SETUP = 0x0a,
 
 };
 
